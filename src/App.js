f351ee99b5b0ed18dd90476afcc90588db6346a9
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-07-29 22:47:40 +0700

    Redux counter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import Counter from './containers/Counter'
+import counterReducer from './redux/counter'
 
-const App = () => <h1>Welcome to react-minimum-starter</h1>
+const store = createStore(
+  counterReducer,
+  /* initial state , */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+const App = () =>
+  <Provider store={store}>
+    <Counter />
+  </Provider>
 export default App
